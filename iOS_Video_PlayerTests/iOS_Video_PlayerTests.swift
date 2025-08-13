e521db8913d9a0fd429f56daf5e1ff1706e36a6b
Tayvon Chamale <zodyzockrage@gmail.com>
2025-08-13 15:16:27 -0600

    Initial commit with Xcode starter project

---
+++ iOS_Video_PlayerTests/iOS_Video_PlayerTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Video_PlayerTests.swift
+//  iOS_Video_PlayerTests
+//
+//  Created by Tayvon Chamale on 8/13/25.
+//
+
+import Testing
+@testable import iOS_Video_Player
+
+struct iOS_Video_PlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
